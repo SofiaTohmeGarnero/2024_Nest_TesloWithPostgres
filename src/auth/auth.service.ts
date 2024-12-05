cd27fe11689ac7e42eee4f10aecee98c6b9e6cc7
Sofia-Tohme-Globant <sofia.tohme@globant.com>
2024-12-05 17:04:14 -0300

    ADD auth resource - create user

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,39 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AuthService {
+  private readonly logger = new Logger('AuthService');
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createAuthDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createAuthDto);
+      await this.userRepository.save(user);
+      return user;
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+  }
+
+  private handleExceptions(error: any): never {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    this.logger.error(error);
+    throw new InternalServerErrorException(
+      'unexpected error, check server logs',
+    );
+  }
+}
