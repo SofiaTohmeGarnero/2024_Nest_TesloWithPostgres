46677cf549044466aa0ff95a2ffd3d0f46b47245
Sofia-Tohme-Globant <sofia.tohme@globant.com>
2024-12-05 18:11:38 -0300

    ADD authentication deps passport and jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,47 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+
+    PassportModule.register({defaultStrategy: 'jwt'}),
+
+    JwtModule.registerAsync({
+      imports: [ ConfigModule ],
+      inject: [ ConfigService ],
+      useFactory: (configService: ConfigService) => {
+        return {
+          secret: configService.get('JWT_SECRET'), //usamos el modulo de configService, pero tmb funcionaría con la var de entorno de manera directa (process.env.JWT_SECRET)
+          signOptions:{
+            expiresIn: '2h'
+          }
+        }
+      }
+    }),
+
+    /*     
+    JwtModule.register({
+      El problema es que puede ser que process.env.JWT_SECRET no esté definido 
+      en el momento en el cual la aplicación está montándose.
+      Entonces sería mejor que este módulo fuera montado de manera asíncrona 
+      y así asegurarme de que siempre voy a tener un valor aquí
+      
+      secret: process.env.JWT_SECRET,
+      signOptions:{
+        expiresIn: '2h'
+      }
+    }) 
+    */
+  ],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
