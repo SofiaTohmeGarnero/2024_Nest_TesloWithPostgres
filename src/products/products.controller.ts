841eb83ebb11c0325fe14ef2ce2a0e3c401d532b
Sofia-Tohme-Globant <sofia.tohme@globant.com>
2024-12-13 14:55:31 -0300

    ADD @ApiProperty() en las columnas de product entity para hacer sync con la doc swagger

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -15,6 +15,8 @@ import { UpdateProductDto } from './dto/update-product.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { Auth, GetUser } from 'src/auth/decorators';
 import { User } from 'src/auth/entities/user.entity';
+import { ApiResponse } from '@nestjs/swagger';
+import { Product } from './entities';
 
 @Controller('products')
 @Auth() //tengo que estar logueado para acceder a todos los endpoints de products
@@ -22,6 +24,13 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'Product was created.',
+    type: Product,
+  })
+  @ApiResponse({ status: 400, description: 'Bad request.' })
+  @ApiResponse({ status: 403, description: 'Forbidden. Token related.' })
   create(@Body() createProductDto: CreateProductDto, @GetUser() user: User) {
     return this.productsService.create(createProductDto, user);
   }
