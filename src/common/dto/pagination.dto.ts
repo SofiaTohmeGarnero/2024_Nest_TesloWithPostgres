d517098e94fba1cb7c4229f26119a1145ef36a07
Sofia-Tohme-Globant <sofia.tohme@globant.com>
2024-12-13 17:15:13 -0300

    ADD @ApiProperty() para documentar dto

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,13 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, IsPositive, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'How many rows do you need',
+  })
   @IsOptional()
   @IsPositive()
   @Min(1)
   @Type(() => Number) //Es como el enableImplicitConversion que se coloca en el main.ts -> useGlobalPipes
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'How many rows do you want to skip',
+  })
   @IsOptional()
   @Min(0)
   @Type(() => Number) //Es como el enableImplicitConversion que se coloca en el main.ts -> useGlobalPipes
--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsArray,
   IsIn,
@@ -14,40 +15,53 @@ import {
  *  - slug es opcional, aunque en la base de datos es obligatorio y como no le colocamos un valor por defecto (pq debe ser unico) usamos la fnc beforeInsert para asegurrarnos que exista y sea conforme lo que necesitamos
  * */
 export class CreateProductDto {
+  @ApiProperty({
+    description: 'Product title (unique)',
+    nullable: false,
+    minLength: 1,
+  })
   @IsString()
   @MinLength(1)
   title: string;
 
+  @ApiProperty()
   @IsNumber()
   @IsPositive()
   @IsOptional()
   price?: number;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   description?: string;
 
+  @ApiProperty()
   @IsString()
   @IsOptional()
   slug?: string;
 
+  @ApiProperty()
   @IsInt()
   @IsPositive()
   @IsOptional()
   stock?: number;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   sizes: string[];
 
+  @ApiProperty()
   @IsIn(['men', 'women', 'kid', 'unisex'])
   gender: string;
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
   tags?: string[];
 
+  @ApiProperty()
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
